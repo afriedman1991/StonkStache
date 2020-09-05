4b7dde75c4e7a84c6d05213828d7a7298e92b8c8
isabiiil <isabelabonitalla@gmail.com>
2020-09-05 15:27:00 -0400

    draft dash

--- src/App.js
+++ src/App.js
@@ -1,92 +1,74 @@
-// import React from "react";
-// import ReactFC from "react-fusioncharts";
-// import FusionCharts from "fusioncharts";
-// import Column2D from "fusioncharts/fusioncharts.charts";
-// import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
-// ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
-
-// const chartData = [
-//   {
-//     label: "Venezuela",
-//     value: "290"
-//   },
-//   {
-//     label: "Saudi",
-//     value: "260"
-//   },
-//   {
-//     label: "Canada",
-//     value: "180"
-//   },
-//   {
-//     label: "Iran",
-//     value: "140"
-//   },
-//   {
-//     label: "Russia",
-//     value: "115"
-//   },
-//   {
-//     label: "UAE",
-//     value: "100"
-//   },
-//   {
-//     label: "US",
-//     value: "30"
-//   },
-//   {
-//     label: "China",
-//     value: "30"
-//   }
-// ];
-
-// const chartConfigs = {
-//   type: "column2d", // The chart type
-//   width: "700", // Width of the chart
-//   height: "400", // Height of the chart
-//   dataFormat: "json", // Data type
-//   dataSource: {
-//     // Chart Configuration
-//     chart: {
-//       //Set the chart caption
-//       caption: "Countries With Most Oil Reserves [2017-18]",
-//       //Set the chart subcaption
-//       subCaption: "In MMbbl = One Million barrels",
-//       //Set the x-axis name
-//       xAxisName: "Country",
-//       //Set the y-axis name
-//       yAxisName: "Reserves (MMbbl)",
-//       numberSuffix: "K",
-//       //Set the theme for your chart
-//       theme: "fusion"
-//     },
-//     // Chart Data
-//     data: chartData
-//   }
-// };
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//     <>
-//       <h1>DEMO GRAPH</h1>
-//       <ReactFC {...chartConfigs} />
-//       <br />
-      
-//     </>);
-//   }
-// }
-
-// export default App;
-
-
-
 import React, { Component } from 'react';
+import { WidthProvider, Responsive } from "react-grid-layout";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Column2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 import './App.css';
-import RGL, { WidthProvider, Responsive } from "react-grid-layout";
-//  import css -- IMP!!!
 import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
+
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const chartData = [
+  {
+    label: "Venezuela",
+    value: "290"
+  },
+  {
+    label: "Saudi",
+    value: "260"
+  },
+  {
+    label: "Canada",
+    value: "180"
+  },
+  {
+    label: "Iran",
+    value: "140"
+  },
+  {
+    label: "Russia",
+    value: "115"
+  },
+  {
+    label: "UAE",
+    value: "100"
+  },
+  {
+    label: "US",
+    value: "30"
+  },
+  {
+    label: "China",
+    value: "30"
+  }
+];
+
+const chartConfigs = {
+  type: "column2d", // The chart type
+  // width: "700", // Width of the chart
+  // height: "400", // Height of the chart
+  dataFormat: "json", // Data type
+  dataSource: {
+    // Chart Configuration
+    chart: {
+      //Set the chart caption
+      caption: "Countries With Most Oil Reserves [2017-18]",
+      //Set the chart subcaption
+      subCaption: "In MMbbl = One Million barrels",
+      //Set the x-axis name
+      xAxisName: "Country",
+      //Set the y-axis name
+      yAxisName: "Reserves (MMbbl)",
+      numberSuffix: "K",
+      //Set the theme for your chart
+      theme: "fusion"
+    },
+    // Chart Data
+    data: chartData
+  }
+};
 
 const ReactGridLayout = WidthProvider(Responsive);
 
@@ -94,12 +76,12 @@ class App extends Component {
   
   state = { 
     layout : [
-      {i: '1', x: 0, y: 0, w: 1, h: 2, minH: 2, maxH: 2},         // *** -- minH & maxH doesnt effect the grid items
-      {i: '2', x: 1, y: 0, w: 1, h: 2, minH: 2, maxH: 2},
-      {i: '3', x: 0, y: 1, w: 1, h: 2, minH: 2, maxH: 2},
+      {i: '1', x: 0, y: 0, w: 3, h: 2, minH: 2, maxH: 2},         // *** -- minH & maxH doesnt affect the grid items
+      {i: '2', x: 1, y: 0, w: 4, h: 2, minH: 2, maxH: 2},
+      {i: '3', x: 0, y: 1, w: 3, h: 2, minH: 2, maxH: 2},
       {i: '4', x: 1, y: 1, w: 1, h: 2, minH: 2, maxH: 2}
     ],
-    resizeplotly: false,
+    resizeplotly: true,
   };
 
   onLayoutChange = (layout) => {
@@ -145,11 +127,11 @@ class App extends Component {
               </div>
               <div style={{marginTop: '80px'}}>Grid - 3</div>
             </div>
-            <div className="item" key={4}>
+            <div className="item" id="item4" key={4}>
               <div className='MyDragHandleClassName'> 
                 Drag from Here - <span className="text">4</span>
               </div>
-              <div style={{marginTop: '80px'}}>Grid - 4</div>
+              <ReactFC {...chartConfigs} className = "graph"/>
             </div>
         </ReactGridLayout>
         
